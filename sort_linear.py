b6016b56fe677bee9c6232cdd2114df0fd868705
dileepvdev <137151186+dileepvdev@users.noreply.github.com>
2023-09-21 15:15:30 +0530

    Add files via upload

---
+++ sort_linear.py
@@ -0,0 +1,9 @@
+def sort_array(arr):
+    return sorted(arr)
+def main():
+    arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+    sorted_arr = sort_array(arr)
+    print(sorted_arr)
+
+if __name__ == '__main__':
+    main()
